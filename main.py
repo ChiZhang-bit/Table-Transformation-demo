6aa36be0cf96bf8c6db10b484a51ecd86c770cca
ChiZhang-bit <1061823234@qq.com>
2023-02-08 11:21:24 +0800

    add utils.py and revision main

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import pandas
+import numpy as np
+import pandas as pd
+import xlrd
+
+from utils import *
+
 
 if __name__ == "__main__":
-    print(1)
+    table = pd.read_excel("asset/Simplified Hierachical Table.xlsx",
+                          header=[0, 1],
+                          index_col=[0, 1])
+
+    table = revision_index(table)
+
+    print(table.index)
+    print(table)
+    print(table[2018]["Spring"].loc['Stars']['KDA'])
+    print(table[2018]["Spring"].loc['Wolves'])
---
+++ utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import xlrd
+
+
+def revision_index(hierarchi_table: pd.DataFrame):
+    """
+    修改层次表的索引，取消NaN值
+    :param hierarchi_table:
+    :return: Dataframe
+    """
+    revision_index = []
+    for i in hierarchi_table.index:
+        if np.nan in i:
+            newlist = []
+            for j in range(len(i)):
+                if i[j] is np.nan:
+                    newlist.append(temp[j])
+                else:
+                    newlist.append(i[j])
+            revision_index.append(tuple(newlist))
+        else:
+            temp = i
+            revision_index.append(i)
+    hierarchi_table.index = pd.MultiIndex.from_tuples(revision_index)
+    return hierarchi_table
